f77689b10dcc8dba70a742616adf76ee942eac9b
Pair <team@scraperwiki.com>
2012-08-09 06:17:12 -0400

    database file name

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
+import os
 import unittest
 import dumptruck
 import dumptruck_web
 
+DB = 'dumptruck.db'
+
 class SqliteApi(unittest.TestCase):
     def setUp(self):
         try:
-            os.remove('dumptruck.db')
+            os.remove(DB)
         except OSError:
             pass
-        self.dt = dumptruck.DumpTruck(dbname="dumptruck.db")
+        self.dt = dumptruck.DumpTruck(dbname=DB)
 
 class TestQueries(SqliteApi):
     test_valid_query(self):
@@ -27,8 +30,29 @@ class TestQueries(SqliteApi):
     test_destructive_query(self):
         observedCode, observedData = dumptruck_web('?q=DROP+TABLE+sqlite_master;')
 
-        self.assertEqual(observedData, 'SQL error: near "chainsaw": syntax error')
+        self.assertEqual(observedData, 'Error: attempt to write a readonly database')
         self.assertEqual(observedCode, 403)
+
+class TestFileness(unittest.TestCase):
+    def setUp(self):
+        try:
+            os.remove(DB)
+        except OSError:
+            pass
+
+    def test_query_nonexistant(self):
+        "When we query a database file that doesn't exist"
+        observedCode, observedData = dumptruck_web('?q=SELECT+3+FROM+sqlite_master')
+
+        # File is not created
+        self.assertFalse(os.path.isfile(DB))
+
+        # Empty list is returned
+        self.assertListEqual(json.loads(observedData), [])
+
+        # All is well.
+        self.assertEqual(observedCode, 200)
+        
 
 #   test_private_db(self):
 #       observedCode, observedData = dumptruck_web('?q=')
