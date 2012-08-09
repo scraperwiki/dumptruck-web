16a68fb350edb729e005b46125d39d7a371b8832
Pair <team@scraperwiki.com>
2012-08-09 06:27:08 -0400

    get query

--- dumptruck_web.py
+++ dumptruck_web.py
@@ -1,6 +1,22 @@
-
+import dumptruck
+import demjson
 
 def dumptruck_web(querystring):
+    dt = dumptruck.DumpTruck()
+
+    # http://docs.python.org/library/cgi.html
+    form = cgi.FieldStorage()
+    if "q" not in form:
+        print 'Error: No query specified'
+
+    try:
+        data = dt.execute(form['q'])
+    except:
+        raise
+    else:
+        code = 200
+
+    return code, demjson.encode(data)
 
 
 
--- test.py
+++ test.py
@@ -16,22 +16,28 @@ class SqliteApi(unittest.TestCase):
 class TestQueries(SqliteApi):
     test_valid_query(self):
         self.dt.insert({'name': 'Aidan', 'color': 'Green'}, 'person')
-        observedCode, observedData = dumptruck_web('?q=SELECT+favorite_color+FROM+person')
+        observedCode, observedData = dumptruck_web('q=SELECT+favorite_color+FROM+person')
 
         self.assertListEqual(json.loads(observedData), [{"name":"Aidan","color":"Green"}])
         self.assertEqual(observedCode, 200)
 
     test_invalid_query(self):
-        observedCode, observedData = dumptruck_web('?q=chainsaw')
+        observedCode, observedData = dumptruck_web('q=chainsaw')
 
         self.assertEqual(observedData, 'SQL error: near "chainsaw": syntax error')
         self.assertEqual(observedCode, 400)
 
     test_destructive_query(self):
-        observedCode, observedData = dumptruck_web('?q=DROP+TABLE+sqlite_master;')
+        observedCode, observedData = dumptruck_web('q=DROP+TABLE+sqlite_master;')
 
         self.assertEqual(observedData, 'Error: attempt to write a readonly database')
         self.assertEqual(observedCode, 403)
+
+    test_no_query(self):
+        observedCode, observedData = dumptruck_web('')
+
+        self.assertEqual(observedData, 'Error: No query specified')
+        self.assertEqual(observedCode, 400)
 
 class TestFileness(unittest.TestCase):
     def setUp(self):
@@ -42,7 +48,7 @@ class TestFileness(unittest.TestCase):
 
     def test_query_nonexistant(self):
         "When we query a database file that doesn't exist"
-        observedCode, observedData = dumptruck_web('?q=SELECT+3+FROM+sqlite_master')
+        observedCode, observedData = dumptruck_web('q=SELECT+3+FROM+sqlite_master')
 
         # File is not created
         self.assertFalse(os.path.isfile(DB))
@@ -55,7 +61,7 @@ class TestFileness(unittest.TestCase):
         
 
 #   test_private_db(self):
-#       observedCode, observedData = dumptruck_web('?q=')
+#       observedCode, observedData = dumptruck_web('q=')
 #       self.assertEqual(observedCode, 401)
 
 if __name__ == '__main__':
