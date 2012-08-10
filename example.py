4f361de6eef3074ba4450795e7c2fc8573fad0c2
Thomas Levine <occurrence@thomaslevine.com>
2012-08-10 05:14:50 -0400

    example.py structure

--- example.py
+++ example.py
@@ -1,14 +1,14 @@
-import os
-from dumptruck_web import sqlite_api
-
 def main():
-    # Settings
     import demjson
+    import os
+    from dumptruck_web import sqlite_api
+
+    # Settings
     sw_json = open(os.path.expanduser('~/sw.json')).read()
     db = os.path.expanduser(demjson.decode(sw_json)['database'])
 
+    # Go
     return sqlite_api(db)
 
 if __name__ == "__main__":
-    http = main()
-    print http
+    print(main())
